72962c7e6c69eb9ba381a632c10bee7bfad3ad16
GUIFRE88 <guilherme.fre073@hotmail.com>
2026-05-25 10:52:13 -0300

    Add scope for querying users by normalized email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   validates :email_address, presence: true
+
+  scope :with_email, ->(email) { where(email_address: email.strip.downcase) }
 end
